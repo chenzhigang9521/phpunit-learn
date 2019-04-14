1970acd8ebab909adc940b37da2a32f5730574c8
chen zhigang <chen_509@126.com>
2019-04-14 19:47:02 +0800

    建立单元测试相关文件

---
+++ tests/ExceptionTest.php
@@ -0,0 +1,13 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ExceptionTest extends TestCase
+{
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testException()
+    {
+    }
+}
+?>
