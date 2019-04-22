90afbb44e38b16f3d2c989f915d777dc01542640
陈志刚 <zhigang.chen@mfashion.com.cn>
2019-04-22 15:50:17 +0800

    增加测试demo

--- tests/subObjectTest/SubjectTest.php
+++ tests/subObjectTest/SubjectTest.php
@@ -25,7 +25,65 @@ class SubjectTest extends TestCase
         // 在 $subject 对象上调用 doSomething() 方法，
         // 预期将以字符串 'something' 为参数调用
         // Observer 仿件对象的 update() 方法。
-        var_dump($subject->doSomething());die;
         $subject->doSomething();
+    }
+
+    public function testErrorReported()
+    {
+        // 为 Observer 类建立仿件，对 reportError() 方法进行模仿
+        $observer = $this->getMockBuilder(Observer::class)
+            ->setMethods(['reportError'])
+            ->getMock();
+
+        $observer->expects($this->once())
+        ->method('reportError')
+        ->with(
+            $this->greaterThan(0),
+            $this->stringContains('Something'),
+            $this->anything()
+        );
+
+        $subject = new Subject('My subject');
+        $subject->attach($observer);
+
+        // doSomethingBad() 方法应当会通过（observer的）reportError()方法
+        // 向 observer 报告错误。
+        $subject->doSomethingBad();
+    }
+
+    public function testCallbackErrorReported()
+    {
+        // 为 Observer 类建立仿件，模仿 reportError() 方法
+        $observer = $this->getMockBuilder(Observer::class)
+            ->setMethods(['reportError'])
+            ->getMock();
+        $observer->expects($this->once())
+            ->method('reportError')
+            ->with(
+                $this->greaterThan(0),
+                $this->stringContains('Something'),
+                $this->callback(function ($subject) {
+                    return is_callable([$subject, 'getName']) &&
+                         $subject->getName() == 'My subject';
+                })
+            );
+        $subject = new Subject('My subject');
+        $subject->attach($observer);
+
+        // doSomethingBad() 方法应当会通过（observer的）reportError()方法
+        // 向 observer 报告错误。
+        $subject->doSomethingBad();
+    }
+
+    public function testIdenticalObjectPassed()
+    {
+        $expectedObject = new stdClass;
+        $mock = $this->getMockBuilder(stdClass::class)
+            ->setMethods(['foo'])
+            ->getMock();
+        $mock->expects($this->once())
+            ->method('foo')
+            ->with($this->identicalTo($expectedObject));
+        $mock->foo($expectedObject);
     }
 }
